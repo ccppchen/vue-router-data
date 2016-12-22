f77c906d40db25eadcb8d3e988558146deac793d
狼族小狈 <1340641314@qq.com>
2016-12-22 16:55:40 +0800

    测试打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-const rollup = require('rollup')
+const { rollup } = require('rollup')
 const babel = require('rollup-plugin-babel')
 
-rollup.rollup({
+
+rollup({
     entry: 'src/index.js',
     plugins: [babel()]
-}).then(function (bundle) {
+}).then((function (bundle) {
     bundle.write({
         format: 'cjs',
         dest: 'dist/vue-router-data.js'
     })
-})
+}))
